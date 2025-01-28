eff6dde26e179df24defd0aaf263f60b2102b6c5
Stanislav <orlandonetworks@gmail.com>
2025-01-28 14:28:44 +0300

    Initial for Nest

---
+++ backend/prisma/migrations/20250128112150_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "discountedPrice" REAL NOT NULL,
+    "sku" TEXT NOT NULL,
+    "imageUrl" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
